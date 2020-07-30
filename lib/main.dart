87aebe599f1fefab265c0c3c4279fd1ee200ea4e
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-07-30 22:20:07 +0530

    added new files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:photo_firebase/screens/places_list_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,20 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
+      title: 'Great Places',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.amber
       ),
-      body: Text("Home"),
+      home: PlacesListScreen(),
     );
   }
 }
